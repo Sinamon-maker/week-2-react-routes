11bf4b393ccde8b8a4a25ce86704255a914f9712
Kate <nikaterina@gmail.com>
2020-05-11 20:49:55 +0300

    4 trying

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -3,11 +3,11 @@ import React from 'react'
 import { Link, useParams } from 'react-router-dom'
 
 const Profile = () => {
-  const { username } = useParams()
+  const { user: username } = useParams()
   return (
     <div>
       <div id="title">Profile</div>
-      <div id="{username}"> {username} </div>
+      <div id="username"> {username} </div>
       <Link to="/dashboard/main"> Go To Main </Link>
       <Link to="/dashboard"> Go To Root </Link>
     </div>
--- client/components/home.js
+++ client/components/home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import Header from './header'
 import Main from './Main'
 import Dashboard from './Dashboard'
@@ -11,10 +11,11 @@ const Home = () => {
       <Header />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          <Route exact path="/dashboard/*" component={() => <Header />} />
-          <Route exact path="/dashboard/" component={() => <Dashboard />} />
-          <Route exact path="/dashboard/main" component={() => <Main />} />
-          <Route exact path="/dashboard/profile/:user" component={() => <Profile />} />
+          <Switch>
+            <Route exact path="/dashboard" component={() => <Dashboard />} />
+            <Route exact path="/dashboard/main" component={() => <Main />} />
+            <Route exact path="/dashboard/profile/:user" component={() => <Profile />} />
+          </Switch>
         </div>
       </div>
     </div>
